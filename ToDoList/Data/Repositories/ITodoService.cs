ef00309ce636e433094347f3bd06b6fd637ede7d
DenChika <eholocation@gmail.com>
2023-01-22 17:27:07 +0500

    feat: add todo page at app

---
+++ ToDoList/Data/Repositories/ITodoService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using ToDoList.Models;
+
+namespace ToDoList.Data.Repositories;
+
+public interface ITodoService
+{
+    public IEnumerable<TodoItem> GetAll();
+    public Task CreateTodoItem(TodoItem item);
+
+    public Task UpdateTodoItem(TodoItem item);
+
+    public Task<IEnumerable<TodoItem>> FindByTitle(string title);
+
+    public Task<IEnumerable<TodoItem>> FindByTime(string time);
+
+    public Task<IEnumerable<TodoItem>> FindDone();
+    
+    public Task DeleteTodoItem(TodoItem item);
+}
---
+++ ToDoList/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models;
+
+public class TodoItem
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string OnTime { get; set; }
+    public bool IsDone { get; set; }
+
+    public bool IsUpdating { get; set; }
+}
