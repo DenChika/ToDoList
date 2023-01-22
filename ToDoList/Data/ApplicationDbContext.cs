c8a538fbc3f2719f8fbab427b8383bbcc84e8dd3
DenChika <eholocation@gmail.com>
2023-01-22 17:27:52 +0500

    feat: add application db context

---
+++ ToDoList/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using ToDoList.Models;
+
+namespace ToDoList.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        Database.EnsureCreated();
+    }
+
+    public DbSet<TodoItem> TodoItems { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TodoItem>().ToTable("TodoItems");
+        base.OnModelCreating(modelBuilder);
+    }
+}
